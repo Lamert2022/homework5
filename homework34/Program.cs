56209afe9182ce3a99627ca9181ea59613f7eba4
Dmitriy <118912417+Lamert2022@users.noreply.github.com>
2022-12-20 14:27:57 +0300

    first commit

---
+++ homework34/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+Console.Write("Введите размер массива: ");
+var str = Console.ReadLine() ?? "";
+
+if (!int.TryParse(str, out var value))
+{
+    Console.Error.WriteLine($"{str} не является числом");
+    return;
+}
+
+int[] arr = new int[value];
+Random rnd = new();
+int posCount = 0;
+
+for (int i = 0; i < value; i++)
+    arr[i] = rnd.Next(100, 999);
+
+Console.Write("Количество чётных чисел в сгенерированном массиве [");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i].ToString() + (i < arr.Length - 1 ? ", " : ""));
+    if ((arr[i] % 2) == 0) posCount++;
+}
+Console.Write($"] равно {posCount}.");
